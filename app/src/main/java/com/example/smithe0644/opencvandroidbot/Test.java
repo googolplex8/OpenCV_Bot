ed79fefc4ccf3aa8342d5617fb599666df7aeb79
williamlin1 <williamlinsfms@gmail.com>
2018-05-22 14:39:49 -0700

    Added in some basic OpenCV stuff

--- app/src/main/java/com/example/smithe0644/opencvandroidbot/Test.java
+++ app/src/main/java/com/example/smithe0644/opencvandroidbot/Test.java
@@ -1,6 +1,57 @@
 package com.example.smithe0644.opencvandroidbot;
-import org.opencv.android.*;
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
 
-public class Test {
+import org.opencv.android.*;
+import org.opencv.core.Mat;
+
+public class Test extends Activity implements CameraBridgeViewBase.CvCameraViewListener{
+
+    OpenCVLoader OpenCvLoader = new OpenCVLoader();
+
+    private BaseLoaderCallback loaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch(status){
+                case LoaderCallbackInterface.SUCCESS:
+                {
+                    Log.i("Desenuts ", "openCv loaded");
+
+                }break;
+                default:
+                {
+                    super.onManagerConnected(status);
+                }
+            }
+        }
+    };
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+
+    }
+
+    @Override
+    public Mat onCameraFrame(Mat inputFrame) {
+        return null;
+    }
     //hello world
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        OpenCvLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_6,this, loaderCallback);
+    }
+
 }
