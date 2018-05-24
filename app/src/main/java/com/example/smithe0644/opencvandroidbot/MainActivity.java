93644556b8817375f00f692950c76b4fb63fd6e8
williamlin1 <williamlinsfms@gmail.com>
2018-05-24 14:54:38 -0700

    Added in picture taking functionality

--- app/src/main/java/com/example/smithe0644/opencvandroidbot/MainActivity.java
+++ app/src/main/java/com/example/smithe0644/opencvandroidbot/MainActivity.java
@@ -1,18 +1,28 @@
 package com.example.smithe0644.opencvandroidbot;
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.hardware.Camera;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
-import android.view.SurfaceView;
 import android.view.View;
 
 import org.opencv.android.*;
 import org.opencv.core.Mat;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MainActivity extends Activity implements CameraBridgeViewBase.CvCameraViewListener{
 
     OpenCVLoader OpenCvLoader = new OpenCVLoader();
     Camera camera;
+
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
 
     private BaseLoaderCallback loaderCallback = new BaseLoaderCallback(this) {
         @Override
@@ -34,6 +44,7 @@ public class MainActivity extends Activity implements CameraBridgeViewBase.CvCam
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
         camera = getCameraInstance();
     }
 
@@ -64,10 +75,53 @@ public class MainActivity extends Activity implements CameraBridgeViewBase.CvCam
     }
 
 
+
+
     @Override
     public void onResume(){
         super.onResume();
         OpenCvLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_6,this, loaderCallback);
     }
+
+    String mCurrentPhotoPath;
+
+    public void picture(View v){
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            saveImageToExternalStorage(imageBitmap);
+        }
+    }
+
+    private void saveImageToExternalStorage(Bitmap finalBitmap) {
+        String root = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).toString();
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File myDir = new File(root + "/saved_images");
+        myDir.mkdirs();
+        String fname = "CVBot" + timeStamp + ".jpg";
+        File file = new File(myDir, fname);
+        if (file.exists())
+            file.delete();
+        try {
+            FileOutputStream out = new FileOutputStream(file);
+            finalBitmap.compress(Bitmap.CompressFormat.JPEG, 90, out);
+            out.flush();
+            out.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
 
 }
